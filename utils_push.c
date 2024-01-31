4fddac06ea6ff5ce14430740b248684ae8224520
conancintonegro <daroldan@student.42malaga.com>
2024-02-01 00:06:05 +0100

    Creacion de error y cambio split en utils

--- utils_push.c
+++ utils_push.c
@@ -6,11 +6,24 @@
 /*   By: davidrol <davidrol@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/31 23:08:04 by davidrol          #+#    #+#             */
-/*   Updated: 2024/01/31 23:47:05 by davidrol         ###   ########.fr       */
+/*   Updated: 2024/02/01 00:05:09 by davidrol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+void parse_array(t_stack **temp, argv[])
+{
+    char **final;
+    int cont;
+
+    cont = 0;
+    final = ft_split(argv, '');
+    if(!final)
+        exit(0);
+    while (final[cont])
+    
+        
+}
 
 t_stack *trans_list(int argc, char **argv[])
 {
