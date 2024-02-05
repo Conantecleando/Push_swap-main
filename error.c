e8a0860e6f6cdf3c23c574a840c3f475c2321b43
conancintonegro <daroldan@student.42malaga.com>
2024-02-05 19:38:02 +0100

    Subimos la nueva funcion error

--- error.c
+++ error.c
@@ -6,13 +6,28 @@
 /*   By: davidrol <davidrol@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 00:02:58 by davidrol          #+#    #+#             */
-/*   Updated: 2024/02/01 00:05:17 by davidrol         ###   ########.fr       */
+/*   Updated: 2024/02/05 19:17:47 by davidrol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void double_number(char **argv)
+void type_error(char **argv)
 {
-    
+int i;
+
+i = 0;
+    while (argv[i])
+    {
+        if (argv[i] >= 0 && argv[i] <= 9 
+        ||arg[i] == ' '||arg[i] == '+'||arg[i] == '-')
+        i++;
+        else
+        {
+        write(2, 'Error\n', 6);
+        exit(EXIT_FAILURE);
+        }
+    }
 }
+
+void double_number (**)
