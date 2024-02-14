2d3ca25eb906ae4c82253c1ca3346e1a1c6df171
conancintonegro <daroldan@student.42malaga.com>
2024-02-14 23:22:55 +0100

    Arreglamos utils con atol y error se crean nueva funcion

--- utils_push.c
+++ utils_push.c
@@ -6,54 +6,52 @@
 /*   By: davidrol <davidrol@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/31 23:08:04 by davidrol          #+#    #+#             */
-/*   Updated: 2024/02/06 12:07:50 by davidrol         ###   ########.fr       */
+/*   Updated: 2024/02/14 23:22:55 by davidrol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-void parse_array(t_stack **temp, argv[])
+
+char **trans_list(int argc, char **argv[])
 {
-    char **final;
-    int cont;
-
-    cont = 0;
-
-    if (argv[cont]== " && )
-    final = ft_split(argv, '');
-    if(!final)
-        exit(0);
-    while (final[cont])
-    
-        
-}
-
-t_stack *trans_list(int argc, char **argv[])
-{
-    t_stack *temp
-    int i;
+    int       i;
     char **list;
 
-    temp = (t_stack *)mallloc(sizeof(t_stack));
-	if (!temp)
-		return (NULL);
     if (argc == 1)
         exit(0);
     i = 0;
     while (i < argc -1)
     {
-        if (*argv[i] == ")
+        if (*argv[i] == 34)
             list = ft_split(*argv[], " ");
             break;
         else
             list[i] = *argv[i];
-            i++;
-    }
-    i = 0;
-    while (list[i]);
-    {
-        temp->value = list[i];
-        temp->index = i;
-        temp->next = NULL;
         i++;
     }
+    type_error(list);
+    double_number_error(list);
+}
+
+long ft_atol(char *n)
+{
+    int  i;
+    long l;
+
+    l = 0;
+    while (*n == ' ' || *n == '\t' || *n == '\n' || \
+			*n == '\r' || *n == '\f' || *n == '\v')
+		n++;
+    if(n == '-' || n == '+')
+    {
+        if(n == '-')
+            i = -1;
+        n++;
+    }
+    while (ft_isdigit(*n))
+    {
+        l = l * 10 +(n - '0');
+        n++;
+    }
+    return (l * i);
 }
