184818d995dd7fad285b95a2c03b9963cb6b0e63
conancintonegro <daroldan@student.42malaga.com>
2024-03-30 13:18:53 +0100

    Cambios en el argv de >  de 2 en vez de igual

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: davidrol <davidrol@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/31 21:49:35 by davidrol          #+#    #+#             */
-/*   Updated: 2024/03/21 19:48:15 by davidrol         ###   ########.fr       */
+/*   Updated: 2024/03/30 11:09:17 by davidrol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ int	main(int argc, char **argv)
 	stack_a = new_stack(NULL);
 	if (argc == 1 || (argc == 2 && !argv[1][0]))
 		return (1);
-	else if (argv >= 2)
+	else if (argv[2] > NULL)
 		trans_list(argc, argv);
 	sort (stack_a, stack_b)
 	if(list_ok(stack_a))
