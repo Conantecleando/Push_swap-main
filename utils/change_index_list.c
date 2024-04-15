85972f97d6f483400c9ea9e8b0d5624133b3e3dc
SrVariable <rojohnibana@gmail.com>
2024-04-15 12:01:54 +0200

    Arreglar errores de compilación
    - Corregido include
    - Eliminado comprobación innecesaria de stack->next
    - Corregido incremento y decremento de índices

--- utils/change_index_list.c
+++ utils/change_index_list.c
@@ -6,18 +6,18 @@
 /*   By: davidrol <davidrol@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/10 00:29:44 by daroldan          #+#    #+#             */
-/*   Updated: 2024/03/30 14:05:19 by davidrol         ###   ########.fr       */
+/*   Updated: 2024/04/15 12:01:27 by ribana-b         ###   ########.com      */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../malaga-42-main/push_swap/Push_swap-main/bin/push_swap.h"
+#include "../src/push_swap.h"
 
 void	new_index(t_stack **stack)
 {
 	t_stack	*temp;
 	int		i;
 
-	if (!*stack || *stack->next != NULL)
+	if (!*stack)
 		return ;
 	temp = *stack;
 	i = -1;
@@ -33,12 +33,12 @@ void	plus_index_list(t_stack **stack)
 {
 	t_stack	*temp;
 
-	if (!*stack || *stack->next != NULL)
+	if (!*stack)
 		return ;
 	temp = *stack;
 	while (temp->next)
 	{
-		temp->index = (temp)->index++;
+		temp->index++;
 		temp = temp->next;
 	}
 	return ;
@@ -48,12 +48,12 @@ void	minus_index_list(t_stack **stack)
 {
 	t_stack	*temp;
 
-	if (!*stack || *stack->next = NULL)
+	if (!*stack)
 		return ;
 	temp = *stack;
 	while (temp->next)
 	{
-		temp->index = (temp)->index--;
+		temp->index--;
 		temp = temp->next;
 	}
 	return ;
