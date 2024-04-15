5f41c6053c45fe5c0e2e43b11307945402f9d805
SrVariable <rojohnibana@gmail.com>
2024-04-15 11:55:38 +0200

    Arreglar errores de compilación
    - Inicializado stack_a a NULL porque se asigna memoria en trans_list
    - Corregido condición cuando tiene más de un argumento (dos contanto
      el programa)
    - Corregido parámetros de las funciones
    - Comentado función que no funciona

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: daroldan < daroldan@student.42malaga.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/31 21:49:35 by davidrol          #+#    #+#             */
-/*   Updated: 2024/04/08 22:40:19 by daroldan         ###   ########.fr       */
+/*   Updated: 2024/04/15 11:55:20 by ribana-b         ###   ########.com      */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,20 +17,14 @@ int	main(int argc, char **argv)
 	t_stack	*stack_a;
 	t_stack	*stack_b;
 
-	stack_a = (t_stack *)malloc(sizeof(t_stack));
-	if (!stack_a)
-		return (NULL);
 	stack_b = NULL;
-	stack_a = new_stack(NULL);
+	stack_a = NULL;
 	if (argc == 1 || (argc == 2 && !argv[1][0]))
 		return (1);
-	else if (argv[2] > NULL)
-		trans_list(argc, argv);
-	sort (stack_a, stack_b);
+	else if (argc > 2)
+		trans_list(argc, argv, &stack_a);
+	sort(&stack_a, &stack_b);
 	if(list_ok(stack_a))
 		return (0);
-	
-	find_max_value(stack_a)
-
-
+	//find_max_value(stack_a)
 }
